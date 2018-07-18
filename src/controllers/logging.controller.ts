bdabedf12681db1c751e276843c578c2e9beef55
groovybytes <christian.huber@groovybytes.at>
2018-07-18 21:01:18 +0200

    fwerwer

---
+++ src/controllers/logging.controller.ts
@@ -0,0 +1,19 @@
+import {Logger} from "winston";
+import {Body, Controller, Inject, Post} from "@nestjs/common";
+import {ConfigService} from "../services/config.service";
+
+
+@Controller('log')
+export class LoggingController {
+
+  constructor(private config: ConfigService,
+              @Inject('Logger') private logger: Logger) {
+
+  }
+
+  @Post()
+  create(@Body() msg:any, severity:string):void {
+    this.logger.log(severity?severity.toLowerCase():"info",JSON.stringify(msg));
+  }
+
+}
